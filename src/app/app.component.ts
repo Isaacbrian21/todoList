5141f3cf4485443444155efa9a442f8a5af2c455
Isaac Brian <ISANT104@ford.com>
2022-10-24 12:01:03 -0300

    Update -> some changes on app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'todoList';
+export class AppComponent  implements OnInit{
+  
+  
+  constructor(public dialog: MatDialog) {}
+
+
+  ngOnInit(): void {
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      width: '300px'
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('closed');
+      
+    })
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DialogComponent } from './dialog/dialog.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import  {MatDialogModule } from '@angular/material/dialog'
+import  {MatDialogModule } from '@angular/material/dialog';
+import { EditComponent } from './dialog2/edit.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DialogComponent
+    DialogComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
